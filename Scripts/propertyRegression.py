e97cfcdd9eae0894975320253d9baee7391a5ab2
bhelfrecht <ben.helfrecht@gmail.com>
2019-11-15 10:56:25 +0100

    Add property regression for structures with energy centering

--- Scripts/propertyRegression.py
+++ Scripts/propertyRegression.py
@@ -21,11 +21,14 @@ parser.add_argument('-p', type=str, default='volume',
 parser.add_argument('-Z', type=int, nargs='+', default=None, 
         help='Space separated atomic numbers of center species')
 parser.add_argument('-kernel', type=str, default='linear', 
-        choices=['linear', 'gaussian', 'laplacian'], help='Kernel type')
-parser.add_argument('-zeta', type=float, default=1, help='SOAP kernel zeta')
+        choices=['linear', 'gaussian', 'laplacian'], 
+        help='Kernel type')
+parser.add_argument('-zeta', type=float, default=1, 
+        help='SOAP kernel zeta')
 parser.add_argument('-sigma', type=float, default=1.0,
         help='Regularization sigma for regression')
-parser.add_argument('-width', type=float, default=1.0, help='Kernel width')
+parser.add_argument('-width', type=float, default=1.0, 
+        help='Kernel width')
 parser.add_argument('-j', type=float, default=1.0E-16,
         help='Value of sparse jitter')
 parser.add_argument('-npca', type=int, default=None,
@@ -42,41 +45,51 @@ parser.add_argument('-project', type=str, default=None,
         help='File containing SOAP file filenames for projection')
 parser.add_argument('-w', type=str, default=None,
         help='File with regression weights for projection')
+parser.add_argument('-mean', type=str, default=None,
+        help='File with property mean for projection')
 
 args = parser.parse_args()
 
-# Do regression if we are not
-# given regression weights
-if args.w is None:
+if args.w is None or not args.env:
 
-    ### PROPERTY EXTRACTION ###
     # Extract structure properties
     sys.stdout.write('Extracting properties...\n')
     al = qp.AtomsReader(args.structure)
     structIdxs, nAtoms, volume, p \
             = SOAPTools.extract_structure_properties(al, args.Z, propName=args.p)
-    
+        
+# Do regression if we are not
+# given regression weights
+if args.w is None:
+
     # Scale property
     if args.p == 'volume':
         p /= nAtoms/3
-
+    
     # Energies
     else:
     
         # Convert to total energy
         p *= nAtoms/3
-    
+
+        # Save mean property
+        g = open('property_mean.dat', 'w')
+        g.write('%.16f' % np.mean(p/nAtoms))
+        g.close()
+
         # Remove mean binding energy
         p -= np.mean(p/nAtoms)*nAtoms
     
         # Convert back to energy per Si
         p /= nAtoms/3
-    
+
     # Shuffle training indices for each iteration
     randomIdxs = np.arange(0, len(structIdxs))
     np.random.shuffle(randomIdxs)
     trainIdxs = randomIdxs[0:args.ntrain]
     testIdxs = randomIdxs[args.ntrain:]
+
+    # TODO: center properties relative to train set?
     
     # Select representative environments
     repIdxs = np.loadtxt(args.idxs, dtype=np.int)
@@ -93,9 +106,8 @@ if args.w is None:
 
     # Build desired kernels
     if args.kernel == 'gaussian':
-        dMM = cdist(repSOAPs[:, 0:args.npca], 
-                repSOAPs[:, 0:args.npca], metric='euclidean')
-        kMM = SOAPTools.gaussianKernel(dMM, args.width)
+        kMM = SOAPTools.build_kernel(repSOAPs[:, 0:args.npca],
+                repSOAPs[:, 0:args.npca], kernel='gaussian', width=args.width)
         kNM = SOAPTools.build_sum_kernel_batch(inputFiles, 
                 repSOAPs[:, 0:args.npca],
                 structIdxs, kernel='gaussian', width=args.width, nc=args.npca)
@@ -106,10 +118,10 @@ if args.w is None:
                     repSOAPs[:, 0:args.npca], 
                     kernel='gaussian', width=args.width, 
                     nc=args.npca, lowmem=args.lowmem, output=args.output)
+
     elif args.kernel == 'laplacian':
-        dMM = cdist(repSOAPs[:, 0:args.npca], 
-                repSOAPs[:, 0:args.npca], metric='cityblock')
-        kMM = SOAPTools.laplacianKernel(dMM, args.width)
+        kMM = SOAPTools.build_kernel(repSOAPs[:, 0:args.npca],
+                repSOAPs[:, 0:args.npca], kernel='gaussian', width=args.width)
         kNM = SOAPTools.build_sum_kernel_batch(inputFiles, 
                 repSOAPs[:, 0:args.npca],
                 structIdxs, kernel='laplacian', width=args.width, nc=args.npca)
@@ -120,9 +132,10 @@ if args.w is None:
                     repSOAPs[:, 0:args.npca],
                     kernel='laplacian', width=args.width, 
                     nc=args.npca, lowmem=args.lowmem, output=args.output)
+
     else: # linear kernel
         kMM = SOAPTools.build_kernel(repSOAPs[:, 0:args.npca], 
-                repSOAPs[:, 0:args.npca], zeta=args.zeta)
+                repSOAPs[:, 0:args.npca], kernel='linear', zeta=args.zeta)
         kNM = SOAPTools.build_sum_kernel_batch(inputFiles, 
                 repSOAPs[:, 0:args.npca], 
                 structIdxs, zeta=args.zeta, nc=args.npca)
@@ -135,6 +148,7 @@ if args.w is None:
                     lowmem=args.lowmem, output=args.output)
     
     kNM = (kNM.T*3/nAtoms).T
+    # TODO: center kernel?
 
     # Header for the output file with parameter information
     # about the regression
@@ -151,9 +165,12 @@ if args.w is None:
                     envKernel=envKernel, output=args.output)
     
     # Save regression output as [True value, predicted value]
-    np.savetxt('%s/yTrain.dat' % args.output, np.column_stack((yTrain, yyTrain)), header=header)
+    np.savetxt('%s/yTrain.dat' % args.output, 
+            np.column_stack((yTrain, yyTrain)), header=header)
     np.savetxt('%s/yTest.dat' % args.output, 
             np.column_stack((yTest, yyTest)), header=header)
+
+    # Save weights
     np.savetxt('%s/w.dat' % args.output, w)
 
 # Projecting from provided weights
@@ -169,24 +186,62 @@ else:
     repSOAPs = SOAPTools.build_repSOAPs(inputFiles, repIdxs)
     projFiles = SOAPTools.read_input(args.project)
 
-    # TODO: sum kernels instead of environment kernels for IZA properties?
+    # Build environment kernels
+    if args.env:
+        if args.kernel == 'gaussian':
+            k = SOAPTools.build_kernel_batch(projFiles, 
+                    repSOAPs[:, 0:args.npca], 
+                    kernel='gaussian', width=args.width, 
+                    nc=args.npca, lowmem=args.lowmem, output=args.output)
+        elif args.kernel == 'laplacian':
+            k = SOAPTools.build_kernel_batch(projFiles, 
+                    repSOAPs[:, 0:args.npca],
+                    kernel='laplacian', width=args.width, 
+                    nc=args.npca, lowmem=args.lowmem, output=args.output)
+        else: # linear kernel
+            k = SOAPTools.build_kernel_batch(projFiles, 
+                    repSOAPs[:, 0:args.npca],
+                    zeta=args.zeta, nc=args.npca, 
+                    lowmem=args.lowmem, output=args.output)
 
-    # Build desired kernels
-    if args.kernel == 'gaussian':
-        k = SOAPTools.build_kernel_batch(projFiles, 
-                repSOAPs[:, 0:args.npca], 
-                kernel='gaussian', width=args.width, 
-                nc=args.npca, lowmem=args.lowmem, output=args.output)
-    elif args.kernel == 'laplacian':
-        k = SOAPTools.build_kernel_batch(projFiles, 
-                repSOAPs[:, 0:args.npca],
-                kernel='laplacian', width=args.width, 
-                nc=args.npca, lowmem=args.lowmem, output=args.output)
-    else: # linear kernel
-        k = SOAPTools.build_kernel_batch(projFiles, 
-                repSOAPs[:, 0:args.npca],
-                zeta=args.zeta, nc=args.npca, 
-                lowmem=args.lowmem, output=args.output)
+    # Build structure kernels
+    else:
+        if args.kernel == 'gaussian':
+            k = SOAPTools.build_sum_kernel_batch(projFiles, 
+                    repSOAPs[:, 0:args.npca], structIdxs, 
+                    kernel='gaussian', width=args.width, nc=args.npca)
+        elif args.kernel == 'laplacian':
+            k = SOAPTools.build_sum_kernel_batch(projFiles, 
+                    repSOAPs[:, 0:args.npca], structIdxs,
+                    kernel='laplacian', width=args.width, nc=args.npca)
+        else: # linear kernel
+            k = SOAPTools.build_sum_kernel_batch(projFiles, 
+                    repSOAPs[:, 0:args.npca], structIdxs,
+                    zeta=args.zeta, nc=args.npca)
+
+        k = (k.T*3/nAtoms).T
 
     # Perform projection with provided weights
-    SOAPTools.property_regression_oos(w, k, output=args.output)
+    if args.env:
+        SOAPTools.property_regression_oos_env(w, k, output=args.output)
+    else:
+        # Scale property
+        if args.p == 'volume':
+            p /= nAtoms/3
+        
+        # Energies
+        else:
+            f = open(args.mean, 'r')
+            pmean = float(f.readline().strip())
+            f.close()
+        
+            # Convert to total energy
+            p *= nAtoms/3
+
+            # Remove mean binding energy
+            p -= pmean*nAtoms
+        
+            # Convert back to energy per Si
+            p /= nAtoms/3
+
+        SOAPTools.property_regression_oos(w, k, p, output=args.output)
