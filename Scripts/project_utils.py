c274a0130e85b0a9de3f43e139a5870bafb60f63
bhelfrecht <ben.helfrecht@gmail.com>
2020-04-02 16:48:02 +0200

    Add new workflow notebooks, keep old notebooks and scripts for now

---
+++ Scripts/project_utils.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import numpy as np
+import h5py
+
+def load_structures_from_hdf5(filename, datasets=None, concatenate=False):
+    """
+        Load structure-based data from an HDF5 file
+    """
+
+    structure_values = []
+
+    f = h5py.File(filename, 'r')
+
+    if datasets is not None:
+        for dataset name in datasets:
+            structure_values.append(f[dataset_name][:])
+    else:
+        for structure_value in f.values():
+            structure_values.append(structure_value)
+
+    f.close()
+
+    if concatenate:
+        structure_values = np.vstack(structure_values)
+
+    return structure_values
+
+def build_representatives_from_hdf5(filename, representative_idxs, split_idxs):
+    """
+        Build a set of representative environments
+    """
+
+    representatives = []
+
+    f = h5py.File(filename, 'r')
+
+    for i in representative_idxs:
+        structure_number = np.nonzero((split_idxs-i) > 0)[0][0]
+        n = i - split_idxs
+        environment_number = n[structure_number-1]
+        representatives.append(f[f'{structure_number}'][environment_number, :])
+
+    f.close()
+
+    representatives = np.asarray(representatives)
+
+    return representatives
