2231692813a1cc924816a636f3ef573471529afe
bhelfrecht <ben.helfrecht@gmail.com>
2020-11-30 20:11:13 +0100

    Compare structures by eigenvalues of the distance matrix of the primitive cell

--- Scripts/check_structures.py
+++ Scripts/check_structures.py
@@ -12,7 +12,7 @@ import spglib as spg
 symprec = 1.0E-3
 xyz_dir = '../Raw_Data/DEEM_330k/XYZ'
 gulp_dir = '../Raw_Data/GULP/DEEM_330k'
-xyz_files = sorted(glob.glob(f'{xyz_dir}/830-833/*.xyz'))
+xyz_files = sorted(glob.glob(f'{xyz_dir}/8*-8*/*.xyz'))
 geometry_file = f'{gulp_dir}/geometry_errors.dat'
 
 g = open(geometry_file, 'w')
@@ -37,8 +37,9 @@ for xdx, xyz_file in enumerate(tqdm(xyz_files)):
             numbers=primitive_xyz_numbers, pbc=True)
 
     d_xyz = primitive_xyz.get_all_distances(mic=True)
-    eig_xyz = np.linalg.eigvals(d_xyz)
-    idxs_xyz = np.argsort(np.abs(eig_xyz))
+    eig_xyz = np.linalg.eigvalsh(d_xyz)
+    #idxs_xyz = np.argsort(np.abs(eig_xyz))
+    idxs_xyz = np.argsort(eig_xyz)[::-1]
 
     # Reduce CIF (optimized structure) to primitive cell
     # (the optimized structures are already primitive, 
@@ -54,12 +55,13 @@ for xdx, xyz_file in enumerate(tqdm(xyz_files)):
             numbers=primitive_cif_numbers, pbc=True)
 
     d_cif = primitive_cif.get_all_distances(mic=True)
-    eig_cif = np.linalg.eigvals(d_cif)
-    idxs_cif = np.argsort(np.abs(eig_cif))
+    eig_cif = np.linalg.eigvalsh(d_cif)
+    #idxs_cif = np.argsort(np.abs(eig_cif))
+    idxs_cif = np.argsort(eig_cif)[::-1]
 
     # Calculate errors in cell and positions
     cell_error = np.linalg.norm(primitive_cif.cell[:] - primitive_xyz.cell[:])
-    positions_error = np.linalg.norm(eig_cif[idxs_cif] - eig_xyz[idxs_xyz])
+    positions_error = np.linalg.norm(eig_cif[idxs_cif] - eig_xyz[idxs_xyz]) / np.sqrt(len(eig_cif))
     g.write(f'\n{structure_id:7d}  {cell_error:15.8f}  {positions_error:15.8f}')
 
 g.close()
