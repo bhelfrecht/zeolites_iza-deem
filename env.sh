dcbce1f057b6a814f35e65895f2ab9af41490da7
Benjamin Helfrecht <ben.helfrecht@gmail.com>
2021-09-19 10:21:09 -0700

    Add script to set environment variables

---
+++ env.sh
@@ -0,0 +1,21 @@
+# Project Dir
+project_dir="/home/ben/Documents/Work/Zeolites/Zeolites_IZA-DEEM"
+
+# Path to matplotlib custom plotting
+export PYTHONPATH=$PYTHONPATH:"$project_dir"/Code/cosmo-tools/python/matplotlib
+
+# Path to additional utilities
+export PYTHONPATH=$PYTHONPATH:"$project_dir"/Code/utils
+
+# Path to GCH
+export PYTHONPATH=$PYTHONPATH:"$project_dir"/Code/GCH
+
+# Path to chemiscope
+export PYTHONPATH=$PYTHONPATH:"$project_dir"/Code/chemiscope
+
+# Path to project utilities
+export PYTHONPATH=$PYTHONPATH:"$project_dir"/Scripts
+
+# Path to GULP
+export PATH=$PATH:"$project_dir"/Code/GULP/gulp-5.1/Src
+export GULP_LIB="$project_dir"/Code/GULP/gulp-5.1/Libraries
